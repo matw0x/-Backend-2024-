24944e918145d769f750861654e148def4cafa2c
matw0x <boltface@mail.ru>
2024-07-17 23:56:54 +0300

    add task_05

---
+++ 05 Вопрос/task_05.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Departament (
+	ID SERIAL PRIMARY KEY,
+	SALARY VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Users (
+	ID SERIAL PRIMARY KEY,
+	SURNAME VARCHAR(25) NOT NULL,
+	DEPARTAMENT_ID INT REFERENCES Departament(ID),
+	SALARY MONEY NOT NULL
+);
+
+INSERT INTO Departament (SALARY)
+VALUES
+('Backend'),
+('DevOps'),
+('Android'),
+('iOS');
+
+INSERT INTO Users (SURNAME, DEPARTAMENT_ID, SALARY)
+VALUES
+('Алексеев', 3, 50000),
+('Петрухин', 3, 60000),
+('Есенин', 2, 70000),
+('Маяковский', 1, 80000),
+('Нортон', 4, 90000),
+('Антропов', 1, 100000),
+('Андреев', 4, 110000),
+('Силантьев', 1, 120000);
