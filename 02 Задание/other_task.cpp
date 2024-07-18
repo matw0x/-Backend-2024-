ccf5585344dc5d1451a99a4afa404aa20d45cbc2
matw0x <boltface@mail.ru>
2024-07-18 11:01:52 +0300

    final additional task_02

--- 02 Задание/other_task.cpp
+++ 02 Задание/other_task.cpp
@@ -1,4 +1,3 @@
-#include <map>
 #include <unordered_map>
 #include <vector>
 #include <fstream>
@@ -7,21 +6,22 @@
 #include <algorithm>
 #include <utility>
 
-bool readFile(const std::string&, std::unordered_map<std::string, int>&);
-void countWords(std::ifstream&, std::unordered_map<std::string, int>&);
+bool readFile(const std::string&, std::unordered_map<std::string, int>&, std::vector<std::pair<std::string, int>>&);
+void countWords(std::ifstream&, std::unordered_map<std::string, int>&, std::vector<std::pair<std::string, int>>&);
 
 int main() {
     const std::string path = "test.txt";
     std::unordered_map<std::string, int> dataFromFile;
+    std::vector<std::pair<std::string, int>> answer;
 
-    auto print = [&dataFromFile]() {
-        for (auto const& pair : dataFromFile) {
+    auto print = [&answer]() {
+        for (auto const& pair : answer) {
             std::cout << pair.first << " : " << pair.second << '\n';
         }
         std::cout << '\n';
     };
 
-    if (readFile(path, dataFromFile)) {
+    if (readFile(path, dataFromFile, answer)) {
         print();
     } else {
         std::cout << "Error\n";
@@ -30,39 +30,32 @@ int main() {
     return 0;
 }
 
-bool readFile(const std::string& pathToFile, std::unordered_map<std::string, int>& dataFromFile) {
+bool readFile(const std::string& pathToFile, std::unordered_map<std::string, int>& dataFromFile, std::vector<std::pair<std::string, int>>& result) {
     std::ifstream file(pathToFile);
 
     if (file && file.is_open() && file.peek() != std::ifstream::traits_type::eof()) {
-        countWords(file, dataFromFile);
+        countWords(file, dataFromFile, result);
         return true;
     }
 
     return false;
 }
 
-void countWords(std::ifstream& file, std::unordered_map<std::string, int>& dataFromFile) {
+void countWords(std::ifstream& file, std::unordered_map<std::string, int>& dataFromFile, std::vector<std::pair<std::string, int>>& modelMap) {
     std::string word;
     while (file >> word) {
         ++dataFromFile[word];
     }
 
-    std::vector<std::pair<std::string, int>> modelMap;
     for (auto const& elementMap : dataFromFile) {
         modelMap.push_back({ elementMap.first, elementMap.second });
     }
 
     std::sort(modelMap.begin(), modelMap.end(),
     [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) { 
-        return a.second > b.second;
+        if (a.second == b.second) {
+            return a.first > b.first;
+       }
+       return a.second > b.second;
     });
-
-    for (auto const& p : modelMap) {
-        std::cout << p.first << " : " << p.second << '\n';
-    }
-    std::cout << "-------------------\n";
-
-    for (auto const& pair : modelMap) {
-        dataFromFile[pair.first] = pair.second;
-    }
 }
